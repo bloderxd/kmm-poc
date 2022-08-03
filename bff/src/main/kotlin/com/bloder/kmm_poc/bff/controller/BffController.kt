0ad5e9582a63862b95893c09a5cca9345ec2b488
mortarifabio <mortari.fabio@gmail.com>
2022-08-03 09:20:06 -0300

    Create BFF base structure

---
+++ bff/src/main/kotlin/com/bloder/kmm_poc/bff/controller/BffController.kt
@@ -0,0 +1,13 @@
+package com.bloder.kmm_poc.bff.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BffController {
+
+    @GetMapping("/helloworld")
+    fun helloWorld(): String {
+        return "Hello World!"
+    }
+}
---
+++ bff/src/test/kotlin/com/bloder/kmm_poc/bff/BffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bloder.kmm_poc.bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
